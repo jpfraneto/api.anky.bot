1facd815f799c1cc132ac9885bc78dc0de45e01c
jpfraneto <jpfraneto@gmail.com>
2024-07-11 16:53:05 -0400

    aloja

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -2,18 +2,27 @@ import axios from "axios";
 import { sleep } from "./time";
 import { NEYNAR_API_KEY } from "../env/server-env";
 
-export async function getUserFromFid (fid: number) {
+export async function getUserFromFid(fid: number, retryCount = 0): Promise<any> {
+    const MAX_RETRIES = 5;
+
     try {
         const options = {
             method: 'GET',
             url: `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
-            headers: {accept: 'application/json', api_key: NEYNAR_API_KEY}
-          };
-        const response = await axios.request(options)
-        return response.data.users[0]
+            headers: { accept: 'application/json', api_key: NEYNAR_API_KEY }
+        };
+        const response = await axios.request(options);
+        return response.data.users[0];
     } catch (error) {
-        console.log("there was an error fetching the user from fid", error)
-        await sleep(1000)
-        getUserFromFid(fid)
+        console.log(`Error fetching user from fid (attempt ${retryCount + 1}):`, error);
+
+        if (retryCount < MAX_RETRIES - 1) {
+            console.log(`Retrying in 1 second... (${MAX_RETRIES - retryCount - 1} attempts left)`);
+            await sleep(1000);
+            return getUserFromFid(fid, retryCount + 1);
+        } else {
+            console.log(`Max retries (${MAX_RETRIES}) reached. Throwing error.`);
+            throw error;
+        }
     }
 }
