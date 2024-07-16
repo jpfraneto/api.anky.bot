78a99acf7fcb0d87451041858efc0f7072e06fbf
jpfraneto <jpfraneto@gmail.com>
2024-07-16 00:00:32 -0400

    aloja

--- src/routes/moxiefolio/index.tsx
+++ src/routes/moxiefolio/index.tsx
@@ -435,6 +435,7 @@ moxiefolioFrame.frame('/this-users-moxiefolio/:fid', async (c) => {
       const totalAllocated = usersMoxiefolio.entries.reduce((acc: number, fanToken) => acc + fanToken.allocation, 0);
       const usersAirdrop = await getUsersAidropAllocation(fid)
       const percentage = Number((100*totalAllocated/Number(usersAirdrop)).toFixed(2))
+      console.log("the percentage is: ", percentage)
       return c.res({
         title: "moxiefolio",
         image: (
@@ -453,13 +454,13 @@ moxiefolioFrame.frame('/this-users-moxiefolio/:fid', async (c) => {
               )}
             </div>
             <div tw="mt-3 flex flex-col justify-center text-xl text-black">
-              <div tw="flex flex-col p-1 items-center bg-purple-300 rounded-xl">
+              <div tw="w-full flex flex-col p-1 items-center bg-purple-300 rounded-xl">
                 <div tw="flex w-full">{percentage}% of airdrop allocated</div>
-                <div tw="flex w-full ">{Math.floor(usersAirdrop.moxieAirdropAmount * percentage)} $moxie</div>
+                <div tw="flex w-full ">{totalAllocated} $moxie</div>
               </div>
-              <div tw="flex flex-col bg-green-300 rounded-xl">
+              <div tw="w-full flex flex-col bg-green-300 rounded-xl">
                 <div tw="flex w-full">{Number((1 - percentage).toFixed(2))}% of airdrop available</div>
-                <div tw="flex w-full">{Math.floor(usersAirdrop.moxieAirdropAmount * (1 - percentage))} $moxie</div>
+                <div tw="flex w-full">{+usersAirdrop - totalAllocated} $moxie</div>
               </div>
   
             </div>
