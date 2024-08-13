c314447ef3f21980da8c93a1045a9d718874e634
jpfraneto <jpfraneto@gmail.com>
2024-08-13 14:35:22 -0400

    aloja

--- src/index.tsx
+++ src/index.tsx
@@ -823,7 +823,7 @@ async function sendProgramaticDmToSubscribers(subscribers: any) {
 //sendProgramaticDmToSubscribers([16098, 18350, 19696, 12785, 198258])
 // sendProgramaticDmToSubscribers([18350])
 
-const usersAloja = ["jpfranet", "bvdaniel"]
+const usersAloja = ["jpfraneto", "bvdaniel"]
 async function addUsersToDb(users: any){
   console.log("the users are: ", users)
   for (let user of users) {
