01c3424131a45cf01d73909b14420e15bc7e2b26
jpfraneto <jpfraneto@gmail.com>
2024-08-15 13:33:51 -0400

    aloja

---
+++ utils/queue/queueConfig.ts
@@ -0,0 +1,27 @@
+// queueConfig.ts
+import { Queue, Worker, Job } from 'bullmq';
+import Redis from 'ioredis';
+import { REDIS_URL } from '../../env/server-env';
+
+
+const connection = new Redis(REDIS_URL);
+
+export const clipQueue = new Queue('clip-creation', { connection });
+
+export const setupWorkers = () => {
+  const clipWorker = new Worker('clip-creation', async (job: Job) => {
+    await processClipJob(job);
+  }, { connection });
+
+  clipWorker.on('completed', (job) => {
+    console.log(`Job ${job.id} has completed`);
+  });
+
+  clipWorker.on('failed', (job, err) => {
+    console.error(`Job ${job?.id} has failed with ${err.message}`);
+  });
+};
+
+async function processClipJob (job: any){
+  return null
+}
