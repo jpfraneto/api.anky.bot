674d1c9e4e73e83b2d6c4e96e34d24587c362d6d
jpfraneto <jpfraneto@gmail.com>
2024-08-22 13:08:04 -0400

    aloja

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-    apps: [{
-      name: 'frames.vibra.so',
-      script: 'npm',
-      args: 'run serve',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      }
-    }]
-  };
+  apps: [{
+    name: 'frames.vibra.so',
+    script: 'node_modules/.bin/tsx',
+    args: 'src/index.tsx',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
